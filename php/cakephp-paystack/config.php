3399c287b3423b0a24dbf1558a9997d7a62a4db4
Kalu Chinonso <tripletens.kc@gmail.com>
2022-08-13 16:50:17 +0100

    Revert "Final upload"
    This reverts commit 71541478e3a30888bd01ef925cc5f84f27c1383e.

--- php/cakephp-paystack/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-define('PAYSTACK_SECRET_KEY', $_ENV['PAYSTACK_SECRET_KEY']);
-define('PAYSTACK_PUBLIC_KEY', $_ENV['PAYSTACK_PUBLIC_KEY']);
